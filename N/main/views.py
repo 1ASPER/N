64a9d3ad03a795db99e11fd93e8e84f15f35cf9a
ASPER <asperthex@gmail.com>
2023-08-11 14:19:57 +0600

    optimized code in views.py

--- N/main/views.py
+++ N/main/views.py
@@ -1,18 +1,11 @@
 from django.shortcuts import render
 from .models import Article
 from PIL import Image
+from typing import List, Any
 
-
-def show_start(request):
-    return render(request, 'main/start.html')
-
-
-def show_home(request):
-    articles = Article.objects.all()
-
-    image_sizes = []  # w*h
-    max_side = 350
-    for article in articles:
+def optimize_image_size(full_content: dict, max_side: int = 350) -> List[list]:
+    image_sizes = []  # [w*h]
+    for article in full_content:
         img = Image.open(article.image)
         width, height = img.size
 
@@ -29,12 +22,16 @@ def show_home(request):
             new_height = height
 
         image_sizes.append([new_width, new_height])
+    return image_sizes
 
-    combined_data = zip(articles, image_sizes)
-    data = {
-        'combined_data': combined_data
-    }
+def show_start(request: Any):
+    return render(request, 'main/start.html')
+
+def show_home(request: Any):
+    articles = Article.objects.all()
+    combined_data = zip(articles, optimize_image_size(articles, max_side=350))
+    data = {'combined_data': combined_data}
     return render(request, 'main/home.html', data)
 
-def show_lorem(request):
+def show_lorem(request: Any):
     return render(request, 'surprise/lorem.html')
